078a8c47312bf05bcfdbc36e39cecfaf9ef60f30
Joshtri <stuffofyos1516@gmail.com>
2024-12-15 13:16:05 +0800

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 // import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
-import ParfumPage from './pages/ParfumPage';
-import LoginPage from './pages/LoginPage';
-import TransaksiPage from './pages/TransaksiPage';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import ParfumPage from "./pages/ParfumPage";
+import LoginPage from "./pages/LoginPage";
+import TransaksiPage from "./pages/TransaksiPage";
+import PenggunaPage from "./pages/PenggunaPage";
 
 function App() {
-
   return (
     <Router>
-
-
-        <Routes>
-          <Route path="/" element={<LoginPage />} /> 
-          <Route path="/parfum" element={<ParfumPage />} /> {/* Halaman parfum */}
-
-          <Route path="/transaksi" element={<TransaksiPage/>}/>
-        </Routes>
-      
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/parfum" element={<ParfumPage />} /> {/* Halaman parfum */}
+        <Route path="/transaksi" element={<TransaksiPage />} />
+        <Route path="/pengguna" element={<PenggunaPage />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
