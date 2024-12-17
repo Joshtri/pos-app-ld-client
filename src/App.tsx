8a0a762c778362e32003e05731141e049634bad8
Joshtri <stuffofyos1516@gmail.com>
2024-12-17 11:46:15 +0800

    ipdating route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ParfumPage from "./pages/ParfumPage";
 import LoginPage from "./pages/LoginPage";
 import TransaksiPage from "./pages/TransaksiPage";
 import PenggunaPage from "./pages/PenggunaPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/parfum" element={<ParfumPage />} /> {/* Halaman parfum */}
         <Route path="/transaksi" element={<TransaksiPage />} />
         <Route path="/pengguna" element={<PenggunaPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
